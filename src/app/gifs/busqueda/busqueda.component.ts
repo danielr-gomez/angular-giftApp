3ac9c4f9409f5ebd80dc3e75653a9132f6c10a0c
danielr-gomez <36383249+danielr-gomez@users.noreply.github.com>
2021-09-10 16:53:41 -0500

    Initial Commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent  {
+
+  //El signo de admiracion de conoce como el Non-null assertion operator
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService){}
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+
+    if(valor.trim().length ==0){
+      return;
+    }
+    this.gifsService.buscarGifs( valor );
+    this.txtBuscar.nativeElement.value='';
+  }
+
+}
